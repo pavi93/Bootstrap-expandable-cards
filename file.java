d97b254ac868ace0cfd90ba58ab07ee33976c6ac
pavi93 <60815416+pavi93@users.noreply.github.com>
2023-09-12 18:05:05 +0530

    file

--- file.java
+++ file.java
@@ -1,16 +1,7 @@
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.FileAttribute;
-import java.nio.file.attribute.FileOwnerAttributeView;
-import java.nio.file.attribute.UserPrincipal;
-import java.nio.file.attribute.UserPrincipalLookupService;
-import java.nio.file.attribute.BasicFileAttributeView;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.DosFileAttributeView;
-import java.nio.file.attribute.DosFileAttributes;
+import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
+import java.nio.file.attribute.PosixFileAttributeView;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Set;
 
 public class SetFilePermissions {
@@ -18,17 +9,22 @@ public class SetFilePermissions {
         String filePath = "example.txt";
 
         // Define the desired permissions (e.g., read, write, execute for owner)
-        Set<PosixFilePermission> permissions = new HashSet<>();
-        permissions.add(PosixFilePermission.OWNER_READ);
-        permissions.add(PosixFilePermission.OWNER_WRITE);
-        permissions.add(PosixFilePermission.OWNER_EXECUTE);
+        String permissions = "rw-r--r--";
 
         try {
-            // Get the file's path
-            Path file = Path.of(filePath);
+            // Create a File object for the file
+            File file = new File(filePath);
 
-            // Set the permissions
-            Files.setPosixFilePermissions(file, permissions);
+            // Get the PosixFileAttributeView to set permissions (works on Unix-like systems)
+            PosixFileAttributeView posixView = file.toPath().getFileAttributeView(PosixFileAttributeView.class);
+            
+            if (posixView != null) {
+                Set<PosixFilePermission> posixPermissions = PosixFilePermissions.fromString(permissions);
+                posixView.setPermissions(posixPermissions);
+                System.out.println("File permissions set successfully.");
+            } else {
+                System.out.println("PosixFileAttributeView is not supported on this system.");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
